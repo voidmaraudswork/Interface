169f8ea763e6053407c9738a1b224716fd8240c8
voidmaraudswork <voidmaraudswork@gmail.com>
2026-04-29 23:53:37 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit as st
-st.components.v1.html(open("your_file.html").read(), height=1000, scrolling=True)
+import streamlit.components.v1 as components
 
+# Make sure the filename matches exactly what is in your GitHub repo
+with open("index.html", "r") as f:
+    html_code = f.read()
+
+components.html(html_code, height=1000, scrolling=True)
